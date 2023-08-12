cd781420d8aa4d0396c9726c84012b010d1165dc
AthleticBabushka <adityaagarwal1606@gmail.com>
2023-08-12 12:59:24 +0530

    added environ access

--- summerizer.py
+++ summerizer.py
@@ -3,21 +3,21 @@ import requests
 import os
 
 api_token = os.environ.get('API_TOKEN')
+model_id = "sshleifer/distilbart-cnn-12-6"
 
 def query(payload, model_id, api_token):
-	headers = {"Authorization": f"Bearer {api_token}"}
+	headers = {"Authorization": f"Bearer {api_token}"}	
+    
 	API_URL = f"https://api-inference.huggingface.co/models/{model_id}"
 	response = requests.post(API_URL, headers=headers, json=payload)
 	return response.json()
 
 
-model_id = "sshleifer/distilbart-cnn-12-6"
-#api_token = "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"
+
+#pi_token = "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"
 
 
 def summerizer(para):
-    data = query(para, model_id, api_token)
-    a = 33
+    data = query(para, model_id, api_token)   
     text = data[0]['summary_text'] 
     return text
-	
